bf53a26b005f5100fc38d37eda22676bb561a0a9
atiempoDeveloper-3 <sistemas5@atiempo.com.co>
2025-11-05 11:04:35 -0500

    Se agrega restriccion para que no pase en 0 cuando genero orden de compra

--- dotacionAT/dotacionAT/settings/local.py
+++ dotacionAT/dotacionAT/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -33,7 +33,7 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 # }
 
 # if os.getenv('DB_ENGINE') == 'postgresql':
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME', 'dotacion_at'),
@@ -69,9 +69,13 @@ STATIC_URL = 'static/'
 INTERNAL_IPS = ['127.0.0.1']
 
 # Asegúrate de que Django puede acceder a los archivos estáticos de todas las aplicaciones
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'applications/ciudades/static'),
 ]
+
 
 CSRF_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
